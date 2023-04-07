eb095234d2dc45e583f48bbd2444e0f2c7d1ffce
stacy-gol <anastasiya.go@dashly.io>
2023-04-07 14:54:55 +0500

    #6 refactored Header and Content/Parts

--- task-2/courseinfo/src/App.js
+++ task-2/courseinfo/src/App.js
@@ -1,27 +1,37 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part}</p>
+    </div>
+  )
+}
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  const exercises = 10
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part='Fundamentals of React'/>
+      <Content part = 'Using props to pass data'/>
+      <Content part = 'State of a component'/> 
     </div>
   )
 }
+
+
 
 export default App
