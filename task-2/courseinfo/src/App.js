ae7c779824813e5b984e5937990cc652606e00aa
stacy-gol <anastasiya.go@dashly.io>
2023-04-10 23:34:32 +0500

    #6 refactoring props drilling

--- task-2/courseinfo/src/App.js
+++ task-2/courseinfo/src/App.js
@@ -17,14 +17,44 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  console.log("props", props)
   return (
     <div>
-      <Part part="Fundamentals of React" exercises={10}/>
-      <Part part="Using props to pass data" exercises={7}/>
-      <Part part="State of a component" exercises={14}/>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   )
 }
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
 const Total = (props) => {
   return (
     <div>
@@ -32,22 +62,5 @@ const Total = (props) => {
     </div>
   );
 };
-
-
-
-const App = () => {
-  const course = 'Half Stack application development'
-  
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content/>
-      <Total exercises={10 + 7 + 14} />
-    </div>
-  )
-}
-
-
 
 export default App
