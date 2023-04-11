09ea77c983f083a61e64c3c0908b11a123c2103b
stacy-gol <anastasiya.go@dashly.io>
2023-04-11 12:33:14 +0500

    #6 done additional props drilling

--- task-2/courseinfo/src/App.js
+++ task-2/courseinfo/src/App.js
@@ -12,7 +12,7 @@ const Part = (props) => {
   return (
     <div className="part">
       <p>
-        {props.part} {props.exercises}
+        {props.part} {props.exercises} <span className="mark">{props.mark}</span>
       </p>
     </div>
   );
@@ -27,7 +27,7 @@ const Content = (props) => {
       <Part part={props.part3} exercises={props.exercises3} />
       Here is render from partData prop
       {props.partsData.map((onePartData) => {
-        return <Part part={onePartData.part} exercises={onePartData.exercises} />
+        return <Part part={onePartData.part} exercises={onePartData.exercises} mark={onePartData.mark}/>
       })}
     </div>
   );
@@ -37,15 +37,18 @@ const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
+  const mark1 = "A+";
   const part2 = "Using props to pass data";
   const exercises2 = 7;
+  const mark2 = "B+";
   const part3 = "State of a component";
   const exercises3 = 14;
+  const mark3 = "A";
 
   const partsData = [
-    { part: part1, exercises: exercises1 },
-    { part: part2, exercises: exercises2 },
-    { part: part3, exercises: exercises3 },
+    { part: part1, exercises: exercises1, mark: mark1},
+    { part: part2, exercises: exercises2, mark: mark2},
+    { part: part3, exercises: exercises3, mark: mark3},
   ];
   return (
     <div>
