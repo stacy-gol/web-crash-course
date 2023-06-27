e0a4b13f17e4000a2ddd96d28b533d409153230b
stacy-gol <anastasiya.go@dashly.io>
2023-06-27 13:33:40 +0500

    exercises 2.4-2.5

--- Task-5/src/Course.js
+++ Task-5/src/Course.js
@@ -1,25 +1,34 @@
+import React from "react";
+
 const Total = (props) => {
-    let totalExercises = props.parts.reduce((sum, item) => sum + item.exercises, 0);
-    return (
-      <div>
+  let totalExercises = props.courses?.parts?.reduce(
+    (sum, item) => sum + item.exercises,
+    0
+  );
+  return (
+    <div>
       <b>total of {totalExercises} exercises</b>
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
 const Course = (props) => {
+  return (
+    <div>
+      {props.courses.map((course) => (
+        <>
+          <h1 key={course.id}>{course.name}</h1>
+          {course?.parts?.map((part) => (
+            <p key={part.id}>
+              {part.name} {part.exercises}
+            </p>
+          ))}
+        </>
+      ))}
 
-    return (
-        <div>
-            <h1>Half Stack application development</h1>
-                {props.course.parts.map(part =>
-                    <p key={part.id}>
-                        {part.name} {part.exercises}
-                    </p>
-                )}
-            <Total parts={props.course.parts} />
-        </div>
-    )
-}
+      <Total courses={props.courses.parts} />
+    </div>
+  );
+};
 
-export default Course
+export default Course;
