412869cfd6784ae0214c15a4b4c5acfbf0c8582d
stacy-gol <anastasiya.go@dashly.io>
2023-07-07 14:41:23 +0500

    Part 2b exercises 2.10 refactoring to components

--- Part2-phonebook/src/App.js
+++ Part2-phonebook/src/App.js
@@ -1,28 +1,30 @@
 import { useState } from 'react'
-import Name from './components/Name'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 
 const App = () => {
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
-  const [personSearched, setSearch] =  useState('')
+  const [personSearched, setSearch] = useState('')
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phone: '040-123456', id: 1 },
     { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ])
-  
+
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.find((person) => person.name === newName)) {
-        window.alert(`${newName} is already added to phonebook`);
-        return false;
+      window.alert(`${newName} is already added to phonebook`);
+      return false;
     }
     const newPerson = {
-      name:newName,
-      phone:newPhone
+      name: newName,
+      phone: newPhone
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
@@ -45,28 +47,17 @@ const App = () => {
     ? persons
     : persons.filter((person) => person.name.includes(personSearched))
 
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <input value={personSearched} onChange={handleSearch}></input>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameAdding}/>
-        </div>
-        <div>
-        phone: <input value={newPhone} onChange={handlePhoneAdding}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter personSearched={personSearched} handleSearch={handleSearch}/>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameAdding={handleNameAdding} 
+      newPhone={newPhone} handlePhoneAdding={handlePhoneAdding} />
       <h2>Numbers</h2>
-      <ul>
-      {personsToShow.map(person =>
-        <Name key={person.id} 
-        name={person.name} phone={person.phone}/>)}
-      </ul>
+      <Persons personsToShow={personsToShow} />
     </div>
-    
+
   )
 }
 
