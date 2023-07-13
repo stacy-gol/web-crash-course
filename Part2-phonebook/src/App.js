2f229f0a233987c6bd8d92bc6274458315d15fd2
stacy-gol <anastasiya.go@dashly.io>
2023-07-13 14:05:19 +0500

    Part 2d ex 2.13 communication with backend module

--- Part2-phonebook/src/App.js
+++ Part2-phonebook/src/App.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
+import personService from './services/persons'
 
 const App = () => {
   const [newName, setNewName] = useState('')
@@ -12,10 +13,10 @@ const App = () => {
   const [persons, setPersons] = useState([])
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -30,10 +31,10 @@ const App = () => {
       name: newName,
       phone: newPhone
     }
-    axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewPhone('')
       })
